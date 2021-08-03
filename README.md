dc3cdd43f6fc7ce1b7cbb386a6ee3dd578ba7277
Shubhamrawat5 <sr856161@gmail.com>
2021-08-03 16:45:04 +0530

    add all files

---
+++ README.md
@@ -0,0 +1,14 @@
+## Resso-playlist-downloader
+
+1. clone repo
+2. npm install
+3. edit playlist url variable and set your playlist link in "resso_playlist.js" file [line 8]
+4. node app.js
+
+-> Now a song folder will be created in the repo folder.
+
+-> If by chance you stop the program in between so no worries as if song is already downloaded then next time it wont be donwloaded again!
+
+-> Playlist info will be extract and all the matching songs will start donwloading!
+
+-> Also there is 5% chance that song's some remix or different same name song will get downloaded...
---
+++ resso_playlist.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const JSSoup = require("jssoup").default;
+const htmlEntities = require("html-entities");
+
+module.exports.getPlaylist = async () => {
+  try {
+    let playlistObj = {};
+    let url = "https://m.resso.app/ZSJgtp4CM/"; //put your playlist url
+    const response = await axios.get(url);
+    let htmlContent = response.data;
+    let soup = new JSSoup(htmlContent);
+
+    //scraping...
+    const playlistHeaderBlock = soup.find("div", "playlist-info");
+    let playlistName = playlistHeaderBlock.find("h1").text.trim();
+    let playlistUser = playlistHeaderBlock.find("h3").text.trim();
+    console.log(playlistName, playlistUser);
+
+    playlistObj.playlist = htmlEntities.decode(playlistName);
+    playlistObj.user = htmlEntities.decode(playlistUser);
+
+    const tracksInfo = soup.findAll("li", "song-item"); //finding all songs info
+    playlistObj.songs = [];
+
+    for (let track of tracksInfo) {
+      let songName = track.find("h3").text;
+      let singerNames = track.find("p").text;
+      singerNames = singerNames.replace(/\s{2,10}/g, ""); //remove spaces
+      songName = songName.replace(/\?|<|>|\*|"|:|\||\/|\\/g, ""); //removing special characters which are not allowed in file name
+      playlistObj.songs.push({
+        name: htmlEntities.decode(songName),
+        singer: htmlEntities.decode(singerNames),
+      });
+    }
+    playlistObj.total = playlistObj.songs.length; //total songs count
+    // console.log(playlistObj);
+    return playlistObj;
+  } catch {
+    return "Some Error";
+  }
+};
